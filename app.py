0658e2a7c4344afb1b6bcf947577406cb394f0ef
omega-mirage <dummystuff121@gmail.com>
2025-03-19 12:18:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request  # Import 'request' here
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,21 +8,21 @@ def index():
     return render_template('index.html')
 
 @app.route('/onezerolang')
-def binarycode():
+def binary_code_one():
     return render_template('one.html')
+
 @app.route('/binary')
-def binarycode():
+def binary_code_two():
     return render_template('two.html')
 
 @app.route('/code')
-def binarycode():
+def binary_code_three():
     return render_template('three.html')
-    
+
 # Hidden endpoint 1
 @app.route('/flag')
 def flag():
     return "Hidden endpoint is not here"
-
 
 # Hidden endpoint 2 (fake endpoint)
 @app.route('/fakeflag')
@@ -33,7 +33,7 @@ def fakeflag():
 @app.route('/secret')
 def secret():
     if 'X-Secret' in request.headers and request.headers['X-Secret'] == 'true':
-        return "Congrats ! You have done it, Password='codeOfLies'"
+        return "Congrats! You have done it, Password='codeOfLies'"
     else:
         return "Access Denied", 403
 

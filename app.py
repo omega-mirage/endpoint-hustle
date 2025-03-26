12310504aec1ba62b2341f5ae3bfe9bea8973f02
omega-mirage <dummystuff121@gmail.com>
2025-03-26 07:15:10 +0000

    dhara's commits

--- app.py
+++ app.py
@@ -21,6 +21,60 @@ def flag():
 @app.route('/fakeflag')
 def fakeflag():
     return "faking the flag"
+#endpoint 1
+@app.route('/REALITY-SHIFT')
+def reality_shift():
+    return render_template('reality_shift.html')
+
+#endpoint 2
+@app.route('/ECHO-SILENCE')
+def echo_silence():
+    return render_template('echo_silence.html')
+
+#endpoint 3
+@app.route('/VEIL-WALKER')
+def veil_walker():
+    return "VEIL-WALKER"
+
+#endpoint 4
+@app.route('/ETHEREAL-DREAM')
+def ethereal_dream():
+    return "ETHEREAL-DREAM"
+
+#endpoint 5
+@app.route('/LABYRINTH-PATH')
+def labrinth_path():
+    return "LABYRINTH-PATH"
+
+#endpoint 6
+@app.route('/ABYSS-GATE')
+def abyss_gate():
+    return "ABYSS-GATE"
+
+#endpoint 7
+@app.route('/REVELATION')
+def revelation():
+    return "THE FLAG MAY BE THIS ENDPOINT ONLY"
+
+#endpoint 8
+@app.route('/THRESHOLD-CODE')
+def threshold_code():
+    return "THRESHOLD-CODE"
+
+#endpoint 9
+@app.route('/ILLUSION-VEIL')
+def illusion_veil():
+    return "ILLUSION-VEIL"
+
+#endpoint 10
+@app.route('/OBLIVION-TRUTH')
+def oblivion_truth():
+    return "OBLIVION-TRUTH"
+
+#endpoint 11
+@app.route('/NIGHTFALL-ECHO')
+def nightfall_echo():
+    return "NIGHTFALL-ECHO"
 
 # Hidden endpoint 3 (requires a specific header)
 @app.route('/secret')
